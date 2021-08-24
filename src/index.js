d69700b2c097738c4549d3ff5c6c8e4b49962385
M <rajeshwar.m@Fiserv.com>
2021-08-25 04:05:51 +0530

    Touches

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1> Hello! Welcome to Cardathon'21</h1>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
+function setSavings()
+{
 
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
